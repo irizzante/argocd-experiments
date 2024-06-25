054fbd76065cff7a8ad95427c4b1fec75173392c
irizzant <i.rizzante@gmail.com>
2024-06-25 22:19:02 +0200

    feat: Add command to retrieve initial admin password

--- templatePatch/kind.sh
+++ templatePatch/kind.sh
@@ -60,6 +60,8 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm upgrade --install argocd --wait -n argocd --create-namespace argo/argo-cd
 
+echo $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
 kubectl wait --for condition=ready pod --namespace argocd --all --timeout 300s
 
 kubectl apply -f platform.yaml
